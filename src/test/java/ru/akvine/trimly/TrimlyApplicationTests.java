11bd329e5556f7f7590a0be945792a8e2a65d26b
akvine <redgooseman55@gmail.com>
2024-08-05 11:42:22 +0300

    init commit

---
+++ src/test/java/ru/akvine/trimly/TrimlyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akvine.trimly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrimlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
